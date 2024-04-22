0823395d88b1f49754f193dd51749591850cc8cf
Swaraj Kaushik <swarajkaushik@gmail.com>
2024-04-22 16:49:54 +0530

    changes

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,82 +1,101 @@
-import { Router } from 'express'
-import User from '../models/user.js'
-import auth from '../middleware/auth.js'
-const router = new Router()
+import { Router } from "express";
+import User from "../models/user.js";
+import auth from "../middleware/auth.js";
+import { compare, hash } from "bcrypt";
+const router = new Router();
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-    try {
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (e) {
-        res.status(400).send()
-    }
-})
+const result = async (email, password) => {
+  const user = await User.findOne({ email });
 
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
+  if (!user) {
+    throw new Error("Unable to login");
+  }
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+  const isMatch = await compare(password, user.password);
 
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
+  return user;
+};
 
-router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await result(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
 
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.delete('/users/me', auth, async (req, res) => {
-    try {
-        await req.user.remove()
-        res.send(req.user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-export default router
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+export default router;
